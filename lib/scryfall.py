daaf830c83fb77d9aa0d74d4e3e90e492772e488
Evan Hunzinger <hnz.evn@gmail.com>
2020-06-05 12:04:08 -0500

    Add library for Scryfall

---
+++ lib/scryfall.py
@@ -0,0 +1,13 @@
+import requests
+from .wrappers import rate_limit
+
+
+SCRYFALL_URL = "https://api.scryfall.com/{path}"
+
+
+@rate_limit(delay=0.1)
+def get_card_exact(card_name):
+    url = SCRYFALL_URL.format(path="cards/named")
+    params = dict(exact=card_name)
+    response = requests.get(url, params)
+    return response
