f1f7dcf28b372f0707860d05408105b0350e5897
Evan Hunzinger <hnz.evn@gmail.com>
2020-06-07 07:06:27 -0500

    Catch DB creation fails and fail silently

--- service/cards.py
+++ service/cards.py
@@ -1,4 +1,5 @@
 from typing import Dict
+import peewee
 from lib import scryfall, logging
 from models import Card
 
@@ -7,20 +8,23 @@ log = logging.get(name='card_service')
 
 
 def create_card(card_json: Dict) -> Card:
-    return Card.create(
-        id=card_json['id'],
-        oracle_id=card_json['oracle_id'],
-        name=card_json['name'],
-        released_at=card_json['released_at'],
-        mana_cost=card_json['mana_cost'],
-        type_line=card_json['type_line'],
-        oracle_text=card_json['oracle_text'],
-        set=card_json['set'],
-        artist=card_json['artist'],
-    )
+    params = dict()
+    for field in Card._meta.fields.keys():
+        params[field] = card_json.get(field, None)
+
+    try:
+        card = Card.create(**params)
+    except peewee.DatabaseError as e:
+        log.warning("Unable to create card with the following params:")
+        log.warning(params)
+        log.warning(e)
+        return None
+
+    log.info("Created card [%s]", card.name)
+
+    return card
 
 
-# TODO: debug logs if we have to fetch from Scryfall
 def get_card(card_name: str) -> Card:
     try:
         card = Card.get(Card.name == card_name)
