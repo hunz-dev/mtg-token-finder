ea8a72c4d3caf98c1ee3a19e8c23481cbb2a1562
Evan Hunzinger <hnz.evn@gmail.com>
2020-06-07 07:07:05 -0500

    Update program flow

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import yaml
 
-from lib import deckbox
-from models import Card
+from lib import deckbox, logging
 from service import cards as cards_service
 from service.namedtuples import Deck
 
 
 DECKS_FILE = "decks.yml"
+log = logging.get('token-lookup')
 
 
-def is_token_maker(card: Card) -> bool:
-    return 'token' in card.oracle_text.lower()
+def check_deck(deck: Deck):
+    # Extract HTML from Deckbox, and parse out individual cards
+    html_extract = deckbox.get_extract(deck.deck_id)
+    card_names = deckbox.parse_cards(html_extract)
+
+    cards = [cards_service.get_card(name) for name in card_names]
+    token_cards = [card for card in cards if card and card.is_token_maker]
+
+    log.info("Found the following cards that use the word 'token':")
+    for card in token_cards:
+        log.info("%s:\n%s", card.name, card.oracle_text)
 
 
 def main():
@@ -19,16 +28,7 @@ def main():
 
     # Load decks from yaml file
     decks = [Deck(**deck_yaml) for deck_yaml in decks_yaml]
-    deck = decks[0]  # TODO: Run on all decks instead of just one
-
-    # Extract HTML from Deckbox, and parse out individual cards
-    html_extract = deckbox.get_extract(deck.deck_id)
-    card_names = deckbox.parse_cards(html_extract)
-
-    cards = [cards_service.get_card(name) for name in card_names]
-    token_cards = [card.name for card in cards if is_token_maker(card)]
-
-    print(token_cards)
+    [check_deck(deck) for deck in decks]
 
 
 if __name__ == '__main__':
