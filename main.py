32f96bc336e6baa88b948a5d08de64dbe5a780be
Evan Hunzinger <hnz.evn@gmail.com>
2020-06-09 23:32:58 -0500

    Add logs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from service.namedtuples import Deck
 
 DECKS_FILE = "decks.yml"
 OUTPUT_FILE = "out/tokens.xlsx"
+
 log = logging.get('token-lookup')
 
 
@@ -15,10 +16,12 @@ def check_deck(deck: Deck):
     html_extract = deckbox.get_extract(deck.deck_id)
     card_names = deckbox.parse_cards(html_extract)
 
+    # Get all Card objects and determine which ones have tokens
     cards = [cards_service.get_card(name) for name in card_names]
-    token_cards = [card for card in cards if card and card.is_token_maker]
+    token_cards = [c for c in cards if c and c.token_type is not None]
 
-    log.info(f"Writing results for [{deck.name}] to [{OUTPUT_FILE}]")
+    # Write results to an Excel workbook
+    log.info("Writing results for [%s] to [%s]", deck.name, OUTPUT_FILE)
     cards_service.write_to_workbook(deck.name, token_cards, OUTPUT_FILE)
 
 
